ee21a68f5ceb667347a1e44e8bafffcb1629faa8
nka21 <naoki.nico@gmail.com>
2024-11-19 14:41:13 +0900

    done task: ABC086 A

--- src/abs/abc086a/main.js
+++ src/abs/abc086a/main.js
@@ -1,6 +1,6 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    input = input.split(" ").map( str => Number(str));
+    console.log(input[0] * input[1] % 2 === 0 ? "Even" : "Odd")
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
