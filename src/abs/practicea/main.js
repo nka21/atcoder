64f71ce86ffe27be1e0e833ae5ad5700877320b2
nka21 <naoki.nico@gmail.com>
2024-11-19 14:40:00 +0900

    done task: Practice A

--- src/abs/practicea/main.js
+++ src/abs/practicea/main.js
@@ -1,6 +1,13 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    const tmp = input[1].split(" ");
+
+    const a = Number(input[0]);
+    const b = Number(tmp[0]);
+    const c = Number(tmp[1]);
+    const s = input[2];
+
+    console.log(`${a+b+c} ${s}`);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
