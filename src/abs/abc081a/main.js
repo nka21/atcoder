9d11cdc5cf9c5331097f9f47c2983108a8d7511e
nka21 <naoki.nico@gmail.com>
2024-11-19 14:47:59 +0900

    done: ABC081 A

--- src/abs/abc081a/main.js
+++ src/abs/abc081a/main.js
@@ -1,6 +1,7 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    input = input.split("");
+    
+    console.log(input.filter((value) => value === "1").length);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
