4c1e0c98cdb3f4556e5b700377ddbabecc9605bc
nka21 <naoki.nico@gmail.com>
2024-11-20 11:51:48 +0900

    done: ABC085 B

--- src/abs/abc085b/main.js
+++ src/abs/abc085b/main.js
@@ -1,6 +1,15 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    const lines = input.split("\n");
+    // 先頭の要素を削除
+    lines.shift();
+    // 最後の要素を削除
+    lines.pop();
+    // 重複する要素を削除
+    const tiers = Array.from(new Set(lines));
+
+    console.log(tiers.length);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+// 25分
