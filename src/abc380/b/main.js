c35e0e19c5a5faf974c9625ac9d2362f70b0ad9d
nka21 <naoki.nico@gmail.com>
2024-11-20 15:45:30 +0900

    done: ABC380 B

--- src/abc380/b/main.js
+++ src/abc380/b/main.js
@@ -1,6 +1,17 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    const tmp = input.trim().split("|");
+
+    // 先頭と末尾の要素を削除
+    tmp.shift();
+    tmp.pop();
+
+    let lineAmount = [];
+
+    for (let i = 0; i < tmp.length; i++) {
+        lineAmount.push(tmp[i].split("").length);
+    }
+    
+    console.log(lineAmount.join(" "));
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
