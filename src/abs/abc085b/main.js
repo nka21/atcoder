4c3b8ae6a8b46f7d70969c04163feffb71fd95d9
nka21 <naoki.nico@gmail.com>
2024-11-20 11:54:40 +0900

    style: ABC085 B

--- src/abs/abc085b/main.js
+++ src/abs/abc085b/main.js
@@ -1,13 +1,14 @@
 function Main(input) {
     const lines = input.split("\n");
-    // 先頭の要素を削除
-    lines.shift();
-    // 最後の要素を削除
-    lines.pop();
-    // 重複する要素を削除
-    const tiers = Array.from(new Set(lines));
 
-    console.log(tiers.length);
+    // 先頭と末尾の要素を削除
+    lines.shift();
+    lines.pop();
+    
+    // 重複する要素を削除し、段数を計算
+    const tiers = Array.from(new Set(lines)).length;
+
+    console.log(tiers);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
