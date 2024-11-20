db59d536f1e58db82e7ac74fd7ca9b4f15323487
nka21 <naoki.nico@gmail.com>
2024-11-20 15:16:59 +0900

    done: ABC380 A 別解 sort(): [1, 100, 22, 31] のように並び替え join(): 配列の全要素を順に連結した文字列にする デフォルトで `,`

--- src/abc380/a/main.js
+++ src/abc380/a/main.js
@@ -1,25 +1,28 @@
 function Main(input) {
-    const digits = input.split("");
-    
-    const digit1Length = digits
-        .filter((digit) => digit === "1").length; // 1
-    const digit2Length = digits
-        .filter((digit) => digit === "2").length; // 2
-    const digit3Length = digits
-        .filter((digit) => digit === "3").length; // 3
-
-    const terms =
-        digit1Length === 1 &&
-        digit2Length === 2 &&
-        digit3Length === 3;
-
-    if (terms) {
-        console.log("Yes");
-    } else {
-        console.log("No");
-    }
+    const num = input.trim().split("").sort().join("");
+    console.log(num)
+    console.log(num === "122333" ? "Yes" : "No");
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
 
-// 12分
+// 12分 最初の解答
+//     const digits = input.split("");
+
+//     const digit1Length = digits
+//         .filter((digit) => digit === "1").length; // 1
+//     const digit2Length = digits
+//         .filter((digit) => digit === "2").length; // 2
+//     const digit3Length = digits
+//         .filter((digit) => digit === "3").length; // 3
+
+//     const terms =
+//         digit1Length === 1 &&
+//         digit2Length === 2 &&
+//         digit3Length === 3;
+
+//     if (terms) {
+//         console.log("Yes");
+//     } else {
+//         console.log("No");
+//     }
