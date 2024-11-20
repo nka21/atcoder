c8c22e03711683effd25a871ca1f65c1107d4a6e
nka21 <naoki.nico@gmail.com>
2024-11-20 15:07:11 +0900

    done: ABC380 A

--- src/abc380/a/main.js
+++ src/abc380/a/main.js
@@ -1,6 +1,25 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    const digits = input.split("");
+    
+    const digit1Length = digits
+        .filter((digit) => digit === "1").length; // 1
+    const digit2Length = digits
+        .filter((digit) => digit === "2").length; // 2
+    const digit3Length = digits
+        .filter((digit) => digit === "3").length; // 3
+
+    const terms =
+        digit1Length === 1 &&
+        digit2Length === 2 &&
+        digit3Length === 3;
+
+    if (terms) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+// 12分
