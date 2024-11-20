6f2a5dd62fee6ff793dea0b177f9799bcc9b6a0e
nka21 <naoki.nico@gmail.com>
2024-11-20 15:48:13 +0900

    done: ABC380 B 別解

--- src/abc380/b/main.js
+++ src/abc380/b/main.js
@@ -1,17 +1,26 @@
 function Main(input) {
-    const tmp = input.trim().split("|");
+    const inputs = input.trim().split("|");
+    const lineAmount = inputs.map((line) => line.length);
 
     // 先頭と末尾の要素を削除
-    tmp.shift();
-    tmp.pop();
+    lineAmount.shift();
+    lineAmount.pop();
 
-    let lineAmount = [];
-
-    for (let i = 0; i < tmp.length; i++) {
-        lineAmount.push(tmp[i].split("").length);
-    }
-    
     console.log(lineAmount.join(" "));
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+    // const tmp = input.trim().split("|");
+
+    // // 先頭と末尾の要素を削除
+    // tmp.shift();
+    // tmp.pop();
+
+    // let lineAmount = [];
+
+    // for (let i = 0; i < tmp.length; i++) {
+    //     lineAmount.push(tmp[i].split("").length);
+    // }
+    
+    // console.log(lineAmount.join(" "));
