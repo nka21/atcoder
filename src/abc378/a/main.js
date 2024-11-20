babb0fa780fcc8990c6250214cd523ddb46f1972
nka21 <naoki.nico@gmail.com>
2024-11-20 21:13:09 +0900

    done: ABC378 A

--- src/abc378/a/main.js
+++ src/abc378/a/main.js
@@ -1,6 +1,21 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    const ballColors = input.trim().split(" ").map(Number);
+
+    let ans = 0;
+
+    for (let i = 1; i <= 4; i++) {
+        const filterColor = ballColors.filter((color) => color === i);
+
+        if (filterColor.length >= 2) {
+            ans++;
+            
+            if (filterColor.length === 4) {
+                ans++;
+            }
+        }
+    }
+
+    console.log(ans);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
