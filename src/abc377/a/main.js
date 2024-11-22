20af4721c0d16109f69c4ed4132780a0df6c8e5c
nka21 <naoki.nico@gmail.com>
2024-11-23 01:45:10 +0900

    done: ABC377 A

--- src/abc377/a/main.js
+++ src/abc377/a/main.js
@@ -1,6 +1,6 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    input = input.trim().split("").sort().join("");
+    console.log(input === "ABC" ? "Yes" : "No");
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
