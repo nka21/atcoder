2e8f1df8b40227b4d0d5272facf6fab834f54031
nka21 <naoki.nico@gmail.com>
2024-11-23 13:10:47 +0900

    done: abc375

--- src/abc375/a/main.js
+++ src/abc375/a/main.js
@@ -1,6 +1,18 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    input = input.trim().split("\n");
+
+    const N = input[0];
+    const S = input[1].split("");
+
+    let cnt = 0;
+
+    for (let i = 0; i < (N - 2); i++) {
+        if (S[i] === "#" && S[i + 2] === "#" && S[i + 1] === ".") {
+            cnt++;
+        }
+    }
+
+    console.log(cnt);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
