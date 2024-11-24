87d3e1439876da4a89c79b24dde3be7995a42242
nka21 <naoki.nico@gmail.com>
2024-11-24 23:48:25 +0900

    done: abc374 a

--- src/abc374/a/main.js
+++ src/abc374/a/main.js
@@ -1,6 +1,6 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    S = input.trim().slice(-3);
+    console.log(S === "san" ? "Yes" : "No");
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
