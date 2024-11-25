4dbb134a0c482840df621fc691be2d37c9ed4cb6
nka21 <naoki.nico@gmail.com>
2024-11-25 19:42:57 +0900

    done: abc372 A

--- src/abc372/a/main.js
+++ src/abc372/a/main.js
@@ -1,6 +1,15 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    const S = input.trim().split("");
+
+    let ans = [];
+
+    for (let i = 0; i < S.length; i++) {
+        if (!(S[i] === ".")) {
+            ans.push(S[i]);
+        }
+    }
+
+    console.log(ans.join(""));
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
