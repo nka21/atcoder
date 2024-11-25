d6871cc9722322e1dd6fb4f8247ee82e733aacec
nka21 <naoki.nico@gmail.com>
2024-11-25 19:21:43 +0900

    done: abc373 A

--- src/abc373/a/main.js
+++ src/abc373/a/main.js
@@ -1,6 +1,16 @@
 function Main(input) {
-    input = input.split("\n");
-    console.log(input);
+    S = input.trim().split("\n");
+    let ans = 0;
+
+    for (let i = 0; i < S.length; i++) {
+        if (i+1 === S[i].length) {
+            ans++;
+        }
+    }
+
+    console.log(ans);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+// 18:55~
