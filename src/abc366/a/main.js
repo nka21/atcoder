910d2bf958602ff75a934b8ace21abf37cdd25f6
nka21 <naoki.nico@gmail.com>
2024-12-04 23:57:38 +0900

    abc366 a

---
+++ src/abc366/a/main.js
@@ -0,0 +1,11 @@
+function Main(input) {
+    const [N, T, A] = input.trim().split(" ").map(Number)
+
+    if ((Math.max(T, A) > N) / 2) {
+        console.log("Yes")
+    } else {
+        console.log("No")
+    }
+}
+
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
