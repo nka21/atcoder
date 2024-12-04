7c9d403244b28d203cab3b910534f837405ae3fa
nka21 <naoki.nico@gmail.com>
2024-12-05 00:02:53 +0900

    abc366 a

--- src/abc366/a/main.js
+++ src/abc366/a/main.js
@@ -1,7 +1,7 @@
 function Main(input) {
     const [N, T, A] = input.trim().split(" ").map(Number)
 
-    if ((Math.max(T, A) > N) / 2) {
+    if (Math.max(T, A) > (N / 2 | 0)) {
         console.log("Yes")
     } else {
         console.log("No")
