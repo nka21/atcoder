7ff3a8a59b8bb765d20178532a450080f75f527f
nka21 <naoki.nico@gmail.com>
2024-12-16 18:11:59 +0900

    60824390

---
+++ src/abc362/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int R,G,B;
+    cin >> R >> G >> B;
+    
+    string C;
+    cin >> C;
+    
+    if (C == "Red") {
+        cout << min(G,B) << endl;
+    } else if (C == "Green") {
+        cout << min(R,B) << endl;
+    } else if (C == "Blue") {
+        cout << min(R,G) << endl;
+    }
+}
