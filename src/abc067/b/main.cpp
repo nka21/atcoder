aa7da6081ceb11b57542cd22b3522c7ff538dd37
nka21 <naoki.nico@gmail.com>
2024-12-21 22:45:20 +0900

    60993334

--- src/abc067/b/main.cpp
+++ src/abc067/b/main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+    int N, K;
+    cin >> N >> K;
 
+    vector<int> l(N);
+
+    // 入力を受け取る
+    for (int i = 0; i < N; i++) {
+        cin >> l[i];
+    }
+
+    // 大きい順にソート
+    sort(l.begin(), l.end(), greater<int>());
+
+    int sum = 0;
+
+    for (int i = 0; i < K; i++) {
+        sum += l[i];
+    }
+
+    cout << sum << endl;
 }
