f853f9f443767af6bfda4fd9111e6e664d26bf36
nka21 <naoki.nico@gmail.com>
2024-12-25 13:21:35 +0900

    61073068

---
+++ src/abc377/a/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    sort(S.begin(), S.end());
+
+    if (S == "ABC")
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
--- src/abc377/a/main.js
+++
@@ -1,6 +0,0 @@
-function Main(input) {
-    input = input.trim().split("").sort().join("");
-    console.log(input === "ABC" ? "Yes" : "No");
-}
-
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
