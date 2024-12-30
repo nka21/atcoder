e40e3b7706841a709b8f395b34de904be0ee8914
nka21 <naoki.nico@gmail.com>
2024-12-30 20:08:45 +0900

    delete

--- src/abs/abc049c/main.js
+++
@@ -1,6 +0,0 @@
-function Main(input) {
-    input = input.split("\n");
-    console.log(input);
-}
-
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
--- src/abs/abc086a/main.js
+++
@@ -1,6 +0,0 @@
-function Main(input) {
-    input = input.split(" ").map( str => Number(str));
-    console.log(input[0] * input[1] % 2 === 0 ? "Even" : "Odd")
-}
-
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
