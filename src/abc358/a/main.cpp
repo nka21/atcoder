be099b5b052c5e03ea3e2a2a08490b655a926447
nka21 <naoki.nico@gmail.com>
2024-12-31 14:51:22 +0900

    61267711

--- src/abc358/a/main.cpp
+++ src/abc358/a/main.cpp
@@ -1,6 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-
+    string s, t;
+    cin >> s >> t;
+    
+    if (s == "AtCoder" && t == "Land") cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
