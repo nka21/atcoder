04ed1afd2e689c22ceb838e92b8e36dfca3cb656
nka21 <naoki.nico@gmail.com>
2025-01-01 00:22:18 +0900

    61283541

--- src/abc215/b/main.cpp
+++ src/abc215/b/main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+    long long n;
+    cin >> n;
     
+    long long num = 0;
+    int k = 0;
+    
+    while (num <= n) {
+        num = pow(2, k);
+        k++;
+    }
+    
+    cout << k-2 << endl;
 }
