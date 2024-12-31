307024557cccca654b7bddcf9dc8e3c5187692ff
nka21 <naoki.nico@gmail.com>
2024-12-31 14:51:06 +0900

    61268909

--- src/abc358/b/main.cpp
+++ src/abc358/b/main.cpp
@@ -1,6 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    int n, a;
+    cin >> n >> a;
+    
+    vector<int> t(n);
+    for (int i = 0; i < n; i++) cin >> t[i];
 
+    int totalTime = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if (totalTime >= t[i]) {
+            totalTime += a;
+        } else {
+            totalTime = t[i] + a;
+        }
+        
+        cout << totalTime << endl;
+    }
 }
