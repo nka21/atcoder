816113627d472f68400b80f761777857568476c2
nka21 <naoki.nico@gmail.com>
2025-01-04 20:02:41 +0900

    61359757

---
+++ src/abc232/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    int a = s[0] - '0';
+    int b = s[2] - '0';
+    
+    cout << a * b << endl;
+}
