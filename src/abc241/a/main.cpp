6e924583b300bf34d7bc201d141c1d0ccf45d83f
nka21 <naoki.nico@gmail.com>
2025-01-07 01:06:12 +0900

    61445911

---
+++ src/abc241/a/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    vector<int> a(10);
+    for (int i = 0; i < 10; i++) cin >> a[i];
+    
+    int ans = 0;
+    
+    for (int i = 0; i < 3; i++) {
+        ans = a[ans];
+    }
+    
+    cout << ans << endl;
+}
