67b3a66f9ff6978794aea71a369f31df8d892793
nka21 <naoki.nico@gmail.com>
2025-01-07 01:54:22 +0900

    61446420

---
+++ src/abc102/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    if (n % 2 == 0) cout << n << endl;
+    else cout << n*2 << endl;
+}
