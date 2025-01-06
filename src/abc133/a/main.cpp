7cb49759ff760a4a31cb435f4c204ca3e1cdff07
nka21 <naoki.nico@gmail.com>
2025-01-07 01:31:59 +0900

    61446201

---
+++ src/abc133/a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+}
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n,a,b;
+    cin >> n >> a >> b;
+
+    if (n*a > b) {
+        cout << b << endl;
+    } else {
+        cout << n*a << endl;
+    }
+}
