f4224b71ce0788df8b8eb0aa2d2945a6dffdf2a6
nka21 <naoki.nico@gmail.com>
2025-01-08 22:19:34 +0900

    61480492

---
+++ src/abc319/b/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n+1; i++) {
+        bool flag = true;
+        
+        for (int j = 1; j <= 9; j++) {
+            if (n % j == 0 && i % (n/j) == 0) {
+                cout << j;
+                flag = false;
+                break;
+            }
+        }
+        
+        if (flag) {
+            cout << '-';
+        }
+    }
+}
