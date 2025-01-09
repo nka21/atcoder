cfec7e03301aba8a618d21c40ae3f8da8b2dd1a5
nka21 <naoki.nico@gmail.com>
2025-01-10 01:54:52 +0900

    61504503

---
+++ abc345/b/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long X;
+    cin >> X;
+    
+    if (X % 10 == 0 || X < 0) {
+        cout << X / 10 << endl;
+    } else {
+        cout << (X / 10) + 1 << endl;
+    }
+}
