a6ad485470af5c98c4e953fff1155d4a283f94fa
nka21 <naoki.nico@gmail.com>
2025-01-12 18:44:51 +0900

    61633576

---
+++ src/abc334/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int b, g;
+    cin >> b >> g;
+    
+    if (b > g) cout << "Bat" << endl;
+    else cout << "Glove" << endl;
+}
