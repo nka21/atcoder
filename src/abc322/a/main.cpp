b2d5365fc77c66baa6dd4aa53b82a69eead45d4d
nka21 <naoki.nico@gmail.com>
2025-01-23 09:55:03 +0900

    61961880

---
+++ src/abc322/a/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    for (int i = 0; i < n-2; i++) {
+        if (s[i] == 'A' && s[i+1] == 'B' && s[i+2] == 'C') {
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
+
+    cout << -1 << endl;
+}
