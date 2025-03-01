646b8c5dde135619444b38be1ceae2784f5ca239
nka21 <naoki.nico@gmail.com>
2025-03-01 15:18:35 +0900

    fix

--- src/abc314/a/main.cpp
+++ src/abc314/a/main.cpp
@@ -1,11 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     string pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
 
     int N;
-    cin >> N;
+    cin     >> N;
     for (int i = 0; i < N + 2; i++)
     {
         cout << pi[i];
