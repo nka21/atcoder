faf457bdca06840304ae4a1864265fb9605a40f8
nka21 <naoki.nico@gmail.com>
2025-03-01 18:47:03 +0900

    63235466

---
+++ src/abc394/b/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<string> s(n);
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+
+    bool is_sort = false;
+    while (!is_sort)
+    {
+        is_sort = true;
+        for (int i = 0; i < s.size() - 1; i++)
+        {
+            if (s[i].size() > s[i + 1].size())
+            {
+                string tmp = s[i];
+                s[i] = s[i + 1];
+                s[i + 1] = tmp;
+                is_sort = false;
+            }
+        }
+    }
+
+    for (string word : s)
+        cout << word;
+
+    return (0);
+}
