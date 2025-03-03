571e3e66d27c4578de86e60f6980be150adc5575
nka21 <naoki.nico@gmail.com>
2025-03-04 00:09:46 +0900

    63371535

---
+++ src/abc395/c/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vector<int> cnt(1000005);
+    int mult = 0;
+
+    const int INF = 1001001009;
+    int ans = INF;
+    int r = 0;
+    for (int l = 0; l < n; l++)
+    {
+        while (r < n && mult == 0)
+        {
+            cnt[a[r]]++;
+            if (cnt[a[r]] == 2)
+                mult++;
+            r++;
+        }
+        if (mult == 0)
+            break;
+        ans = min(ans, r - l);
+        if (cnt[a[l]] == 2)
+            mult--;
+        cnt[a[l]]--;
+    }
+    if (ans == INF)
+        ans = -1;
+    cout << ans << endl;
+}
