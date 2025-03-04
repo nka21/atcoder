1935ef6cde780589db6deb5c6623537e7ff11327
nka21 <naoki.nico@gmail.com>
2025-03-05 00:27:36 +0900

    63397810

---
+++ src/abc266/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int middle = s.size() / 2;
+    cout << s[middle] << endl;
+}
