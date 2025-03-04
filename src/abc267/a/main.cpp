1cb8043543ca75c03848fb286af78e8993eb1c37
nka21 <naoki.nico@gmail.com>
2025-03-05 00:31:34 +0900

    63397893

---
+++ src/abc267/a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    if (s == "Friday")
+        cout << 1 << endl;
+    if (s == "Thursday")
+        cout << 2 << endl;
+    if (s == "Wednesday")
+        cout << 3 << endl;
+    if (s == "Tuesday")
+        cout << 4 << endl;
+    if (s == "Monday")
+        cout << 5 << endl;
+}
