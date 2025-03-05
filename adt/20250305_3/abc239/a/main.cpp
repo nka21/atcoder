6ff931a1f591ce4ec804b705394fa0e3853cb72f
nka21 <naoki.nico@gmail.com>
2025-03-05 21:09:55 +0900

    adt

---
+++ adt/20250305_3/abc239/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ll h;
+    cin >> h;
+
+    cout << fixed << setprecision(6) << endl;
+    cout << sqrt(h * (12800000 + h)) << endl;
+}
