3df756f1dbbcbb28f22c8aefe0facb404b1c3015
nka21 <naoki.nico@gmail.com>
2025-03-06 14:34:43 +0900

    63431085

--- abc/abc311/a/main.cpp
+++ abc/abc311/a/main.cpp
@@ -1,6 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
+    int n;
+    string s;
+    cin >> n >> s;
 
+    int a = 0, b = 0, c = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'A')
+            a++;
+        if (s[i] == 'B')
+            b++;
+        if (s[i] == 'C')
+            c++;
+
+        if (a >= 1 && b >= 1 && c >= 1)
+        {
+            cout << i + 1 << endl;
+            return (0);
+        }
+    }
 }
