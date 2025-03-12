c0a66e9788c0ca7f086abadbe9a0b550d961f009
nka21 <naoki.nico@gmail.com>
2025-03-13 01:12:07 +0900

    63686290

---
+++ abc/abc306/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << s[i] << s[i];
+    }
+}
